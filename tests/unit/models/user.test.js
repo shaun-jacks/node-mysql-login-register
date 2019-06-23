ac6eca740ba3e9d2898662fb84f88726356a0fd8
Shaun Jackson <sjackson95@gmail.com>
2019-06-23 14:40:46 -0700

    added unit test for generatign auth token, and congif for test database

---
+++ tests/unit/models/user.test.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const db = require("../../../models/index");
+
+describe("user.generateAuthToken", () => {
+  it("should return a valid JWT", async () => {
+    // Create new user
+    const user = await db.User.create({
+      username: "testuser",
+      email: "testemail",
+      password: "testpass"
+    });
+    const payload = { id: user.id, username: user.username };
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    expect(decoded).toMatchObject(payload);
+    await user.destroy({ force: true });
+  });
+});
